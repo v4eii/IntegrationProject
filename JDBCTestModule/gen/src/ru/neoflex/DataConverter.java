5da1f6e4f08e5c31da3b0e5d5abd5421f6cfc7fe
v4eii <evteev.vs36@gmail.com>
2020-04-22 16:32:08 +0300

    JDBC-adapter add

--- JDBCTestModule/gen/src/ru/neoflex/DataConverter.java
+++ JDBCTestModule/gen/src/ru/neoflex/DataConverter.java
@@ -8,7 +8,7 @@ public class DataConverter {
 	 * Sample method that can be called from a Mapping Custom Java transform.
 	 * The content of this method provides the implementation for the Custom Java transform.
 	 */
-	public static BigDecimal sampleTransform(Integer price) {
+	public static BigDecimal sampleTransform(int price) {
 		return new BigDecimal(price);
 		
 	}
